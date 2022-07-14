4f7367fcc9fd6308bc3b9bb2e7f43f8a05f7f38d
Omar Farah <omaarfaarah2019@gmail.com>
2022-07-14 19:38:02 -0400

    add log out

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,20 +5,38 @@ const NavBar = ({ user, handleLogout }) => {
   return (
     <>
       {user ?
-        <nav>
-          <ul>
-            <li><Link to="/meals">MEALS</Link></li>
-            <li><Link to="/workout">WORKOUT</Link></li>
-            <li><Link to="/profile">PROFILE</Link></li>
-            <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
-            <li>
-              <form className="d-flex" role="search">
-                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                <button className="btn btn-outline-success" type="submit">Search</button>
-              </form>
-            </li>
-          </ul>
-        </nav>
+        <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="/">Fitness Bros</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/meals">Meals</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/workouts">Workouts</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/profile">Profile</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" onClick={handleLogout}>Log Out</a>
+              </li>
+            </ul>
+            <form class="d-flex" role="search">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+
       : 
         <nav class="navbar navbar-expand-lg bg-light">
           <div class="container-fluid">
@@ -39,6 +57,9 @@ const NavBar = ({ user, handleLogout }) => {
                 </li>
                 <li class="nav-item">
                   <a class="nav-link" href="/profile">Profile</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/login">Log In</a>
                 </li>
               </ul>
               <form class="d-flex" role="search">
