7b432721848c6254125c47954697efec724c4a1c
Omar Farah <omaarfaarah2019@gmail.com>
2022-07-14 18:45:18 -0400

    add nav bar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,24 +7,59 @@ const NavBar = ({ user, handleLogout }) => {
       {user ?
         <nav>
           <ul>
-            <Link to="/meals">MEALS</Link>
-            <Link to="/workout">WORKOUT</Link>
-            <Link to="/profile">PROFILE</Link>
-            <Link to="/meals">Meals</Link>
-            <Link to="" onClick={handleLogout}>LOG OUT</Link>
+            <li><Link to="/meals">MEALS</Link></li>
+            <li><Link to="/workout">WORKOUT</Link></li>
+            <li><Link to="/profile">PROFILE</Link></li>
+            <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
+            <li>
+              <form className="d-flex" role="search">
+                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                <button className="btn btn-outline-success" type="submit">Search</button>
+              </form>
+            </li>
           </ul>
         </nav>
-      :
-        <nav>
-          <ul>
-            <Link to="/login">Log In</Link>
-            <Link to="/signup">Sign up</Link>
-            <Link to="/meals">Meals</Link>
-          </ul>
+      : 
+        <nav class="navbar navbar-expand-lg bg-light">
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/meals">Meals</a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Dropdown
+                  </a>
+                  <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><hr class="dropdown-divider" /></li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                  </ul>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link disabled">Disabled</a>
+                </li>
+              </ul>
+              <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                <button class="btn btn-outline-success" type="submit">Search</button>
+              </form>
+            </div>
+          </div>
         </nav>
       }
     </>
   )
 }
+
 
 export default NavBar
