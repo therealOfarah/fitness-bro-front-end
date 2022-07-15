b706e2c1210b48cefc4a55b3a9642f53de4f8572
Omar Farah <omaarfaarah2019@gmail.com>
2022-07-15 11:27:03 -0400

    fix search

--- src/services/workoutService.js
+++ src/services/workoutService.js
@@ -1,8 +1,10 @@
 import * as tokenService from '../services/tokenService'
 
 const BASE_URL = `${process.env.REACT_APP_BACK_END_SERVER_URL}/api/workouts`
-
+console.log(process.env.REACT_APP_BACK_END_SERVER_URL)
 export async function workoutSearch(formData){
-  const res = await fetch(`${BASE_URL}?muscle=${formData.query}`)
-  return res.json()
+  const res = await fetch(`http://localhost:3001/api/workouts?search=${formData.query}`)
+  const data =  await res.json()
+  console.log(data)
+  return data
 }
