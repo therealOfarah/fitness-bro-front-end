dd06ec6ef19c765b439438c5211857c0f5cbb485
Omar Farah <omaarfaarah2019@gmail.com>
2022-07-16 15:18:14 -0400

    add meal search

--- src/pages/Meals/Meals.jsx
+++ src/pages/Meals/Meals.jsx
@@ -1,22 +1,34 @@
 import { useState } from "react"
-import Search from "../../Search/Search"
-import { mealsSearch } from '../../services/mealService'
+import MealSearch from "../MealSearch/MealSearch"
+import { mealSearch } from '../../services/mealService'
 
 
 const Meals = props => {
   const[meals,setMeals]= useState([])
 
   const handleMealSearch = async FormData => {
-    const mealResults = await mealsSearch(FormData)
+    const mealResults = await mealSearch(FormData)
     setMeals(mealResults)
   }
 
-  console.log(meals)
+  // console.log(meals)
   return (
+    <>
     <main >
       <h2>Meals works</h2>
-      <Search handleMealSearch={handleMealSearch}/>
+      <MealSearch handleMealSearch={handleMealSearch}/>
+      {meals.map(meal =>
+      <>
+        <h3>Name:{meal?.name}</h3>
+        <h3>Calories:{meal?.calories}g</h3>
+        <h3>Carbohydrates:{meal?.carbohydrates_total_g}g</h3>
+        <h3>Protein:{meal?.protein_g}g</h3>
+        <h3>Fats:{meal?.fat_total_g}g</h3>
+        <h3>Recomended Serviing Size:{meal?.serving_size_g}g</h3>
+      </>
+        )}
     </main>
+    </>
   )
 }
 
--- src/services/mealService.js
+++ src/services/mealService.js
@@ -1,11 +1,9 @@
 import * as tokenService from '../services/tokenService'
 
 const BASE_URL = `${process.env.REACT_APP_BACK_END_SERVER_URL}/api/meals`
-console.log(process.env.REACT_APP_BACK_END_SERVER_URL)
 
-export async function mealsSearch(formData){
-  const res = await fetch(`http://localhost:3001/api/meals?search=${formData.query}`)
+export async function mealSearch(formData){
+  const res = await fetch(`${BASE_URL}?search=${formData.query}`)
   const data =  await res.json()
-  console.log(data)
   return data
 }
