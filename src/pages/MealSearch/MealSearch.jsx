bcc8e2f2208100105f360a5b81f2beeb297ad391
Omar Farah <omaarfaarah2019@gmail.com>
2022-07-16 15:48:12 -0400

    add search otpion

---
+++ src/pages/MealSearch/MealSearch.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react"
+
+const MealSearch = (props) => {
+  const [formData, setFormData] = useState({query: ''})
+
+  const handleChange = evt => {
+    setFormData({ ...formData,[evt.target.name]: evt.target.value})
+  }
+
+  const handleSubmit = evt => {
+    evt.preventDefault()
+    props.handleMealSearch(formData)
+  }
+  return (
+    <>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input 
+            name="query" 
+            type="text"  
+            autoComplete="off"
+            value={formData.query}
+            onChange={handleChange}
+            />
+          <button type="submit">Search</button>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default MealSearch
