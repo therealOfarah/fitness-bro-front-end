c3dc725f8f1c9579c6b7b5e5d20b11ee662250b8
Brian4755 <brian.mccune1@yahoo.com>
2022-07-18 11:12:50 -0500

    remove button with functionality for workout

--- src/pages/ProfileDetails/ProfileDetails.jsx
+++ src/pages/ProfileDetails/ProfileDetails.jsx
@@ -5,22 +5,32 @@ import * as profileService from '../../services/profileService'
 
 const ProfileDetails = (props) => {
 
-  const [profile, setProfile] = useState([])
+  const [profile, setProfile] = useState()
+  const [workouts, setWorkouts] = useState([])
+  const [meals, setMeals] = useState([])
   const { id } = useParams()
 
   useEffect(() => {
     const fetchProfile = async () => {
       const profileData = await profileService.getProfileDetails(id)
       setProfile(profileData)
+      setWorkouts(profileData.workouts)
     }
     fetchProfile()
-  }, [])
+  }, [id])
   console.log(profile)
-  const handleDelete = async (workout) =>{
-    // console.log(profile.workouts)
-    const deletedWorkout = await profileService.deleteWorkout(workout)
-    setProfile(profile.filter((workouts) => workouts.id !== deletedWorkout.id))
+
+  const handleDeleteWorkout = (id) => {
+    setWorkouts(workouts.filter((workout) => workout._id !== id))
     }
+
+    const handleDeleteMeal = (id) => {
+      setMeals(meals.filter((meal) => meal._id !== id))
+    }
+
+    console.log(meals, 'here')
+    console.log(workouts)
+
   return ( 
     <>
       <h1>Profile Details</h1>
@@ -28,20 +38,20 @@ const ProfileDetails = (props) => {
         <div class="card-body">
           <h3 class="card-title">Name: {profile?.name}</h3>
           <h3>{profile?.email}</h3>
-            {profile.workouts?.map(workout =>
+            {workouts?.map(workout =>
             <>
             <div className="col-sm-6">
               <div className="card">
                 <div className="card-body">
                   <h5 className="card-title">{workout.name}</h5>
                   <p className="card-text">{workout.muscle}</p>
-                  <button onClick={() => handleDelete(profile.workouts)} type="button" className="btn btn-danger">Remove</button>
+                  <button onClick={() => handleDeleteWorkout(workout._id)} type="button" className="btn btn-danger">Remove</button>
                 </div>
               </div>
             </div>
             </>
             )} 
-            {profile.meals?.map(meal =>
+            {profile?.meals?.map(meal =>
             <>
             <div className="col-sm-6">
               <div className="card">
@@ -49,7 +59,7 @@ const ProfileDetails = (props) => {
                   <h5 className="card-title">{meal}</h5>
                   <h3>okay</h3>
                   <p className="card-text">{meal.name}</p>
-                  <button onClick={() => handleDelete(profile.meals)} type="button" className="btn btn-danger">Remove</button>
+                  <button onClick={() => handleDeleteMeal(profile.meals)} type="button" className="btn btn-danger">Remove</button>
                 </div>
               </div>
             </div>
