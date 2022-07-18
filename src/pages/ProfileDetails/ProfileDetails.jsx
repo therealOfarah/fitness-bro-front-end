fdfe35ed5ded2a35feaa195383893aebb7a44ce8
Omar Farah <omaarfaarah2019@gmail.com>
2022-07-18 16:45:20 -0400

    start delete functionality

--- src/pages/ProfileDetails/ProfileDetails.jsx
+++ src/pages/ProfileDetails/ProfileDetails.jsx
@@ -21,12 +21,14 @@ const ProfileDetails = (props) => {
     fetchProfile()
   }, [id])
   console.log(profile)
-  const handleDeleteWorkout = (id) => {
-    setWorkouts(workouts.filter((workout) => workout._id !== id))
+  const handleDeleteWorkout = async (id) => {
+    const deletedWorkout = await profileService.deleteWorkout(id)
+    setWorkouts(workouts.filter((workout) => workout._id !== deletedWorkout))
     }
 
-    const handleDeleteMeal = (id) => {
-      setMeal(meals.filter((meal) => meal._id !== id))
+    const handleDeleteMeal = async(id) => {
+      const deletedMeal = await profileService.deletedMeal(id)
+      setMeal(meals.filter((meal) => meal._id !== deletedMeal))
     }
   return ( 
     <>
@@ -78,33 +80,33 @@ const ProfileDetails = (props) => {
             </>
             :
             <section>
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-5 col-md-6 col-12 pb-4">
-                <h1>Comments</h1>
-                <div class="comment mt-4 text-justify float-left">
-                    <img src="https://i.imgur.com/yTFUilP.jpg" alt="" class="rounded-circle" width="40" height="40"/>
-                    <h4>Jhon Doe</h4>
-                    <span>- 20 October, 2018</span>
-                    <br/>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus numquam assumenda hic aliquam vero sequi velit molestias doloremque molestiae dicta?</p>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-5 col-md-6 col-12 pb-4">
+                            <h1>Comments</h1>
+                            <div class="comment mt-4 text-justify float-left">
+                                <img src="https://i.imgur.com/yTFUilP.jpg" alt="" class="rounded-circle" width="40" height="40"/>
+                                <h4>Jhon Doe</h4>
+                                <span>- 20 October, 2018</span>
+                                <br/>
+                                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus numquam assumenda hic aliquam vero sequi velit molestias doloremque molestiae dicta?</p>
+                            </div>
+                            <div class="col-lg-4 col-md-5 col-sm-4 offset-md-1 offset-sm-1 col-12 mt-4">
+                            <form id="algin-form">
+                                <div class="form-group">
+                                    <h4>Leave a comment</h4>
+                                    <label for="message">Message</label>
+                                    <textarea name="msg" id=""msg cols="30" rows="5" class="form-control" ></textarea>
+                                </div>
+                                <div class="form-group">
+                                    <button type="button" id="post" class="btn">Post Comment</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                  </div>
                 </div>
-            <div class="col-lg-4 col-md-5 col-sm-4 offset-md-1 offset-sm-1 col-12 mt-4">
-                <form id="algin-form">
-                    <div class="form-group">
-                        <h4>Leave a comment</h4>
-                        <label for="message">Message</label>
-                        <textarea name="msg" id=""msg cols="30" rows="5" class="form-control" ></textarea>
-                    </div>
-                    <div class="form-group">
-                        <button type="button" id="post" class="btn">Post Comment</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-    </div>
-</section>
+            </section>
             }
           {/* {profile?.workouts?.workout} */}
         </div>
