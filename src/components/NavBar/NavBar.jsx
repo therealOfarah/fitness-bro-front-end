858c4eb59fa21782ac45ec159de5020bc2d2879f
Brian4755 <brian.mccune1@yahoo.com>
2022-07-19 10:46:12 -0500

    landing page color and styling

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -9,7 +9,7 @@ const NavBar = ({ user, handleLogout }) => {
   return (
     <>
     {user ?
-      <Navbar bg="dark" variant="dark" expand="lg">
+      <Navbar  variant="dark" expand="lg">
         <Container>
           <Navbar.Brand>Fitness Bros</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
