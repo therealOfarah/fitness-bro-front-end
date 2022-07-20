7dc4abf7dd004240a032bf701806b63be74a4ad0
Omar Farah <omaarfaarah2019@gmail.com>
2022-07-19 21:32:58 -0400

    add reviews

--- src/services/workoutService.js
+++ src/services/workoutService.js
@@ -30,3 +30,15 @@ export async function addExerciseDetail(exerciseDetail) {
   const data =  await res.json()
   return data
 }
+
+export async function create(form,profileId) {
+  console.log(profileId)
+  const res = await fetch(`${BASE_URL}/${profileId}/reviews`, {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${tokenService.getToken()}`,
+      'Content-Type': 'application/json'}, 
+      body: JSON.stringify(form)
+    })
+  return res.json()
+}
