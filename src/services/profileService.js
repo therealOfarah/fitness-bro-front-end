2eea11da4c44a0dea71af537e016f783bfe68296
Omar Farah <omaarfaarah2019@gmail.com>
2022-07-21 19:50:32 -0400

    fix

--- src/services/profileService.js
+++ src/services/profileService.js
@@ -1,6 +1,6 @@
 import * as tokenService from '../services/tokenService'
 
-const BASE_URL = `${process.env.REACT_APP_BACK_END_SERVER_URL}`
+const BASE_URL = `${process.env.REACT_APP_BACK_END_SERVER_URL}/api/profiles`
 
 async function getAllProfiles() {
   const res = await fetch(`${BASE_URL}`, {
